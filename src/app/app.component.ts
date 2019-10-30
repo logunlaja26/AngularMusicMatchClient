5f406f1e488876d5076fc080b355188ddbf5376e
Ly Ogunlaja <logunlaja26@gmail.com>
2019-10-30 18:55:36 -0500

    Added and configured search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Get Your Favorite Lyrics';
+  title = 'Favorite Lyrics App';
 }
